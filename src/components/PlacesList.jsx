9d4725c43414162db8a221283e19f2eb49390c53
Christian Kuechler <ckuechler@gmail.com>
2024-05-30 17:20:15 +0200

    Placelist and details design adjustments and displaying images

--- src/components/PlacesList.jsx
+++ src/components/PlacesList.jsx
@@ -8,17 +8,15 @@ function PlacesList() {
 
   const API_URL = import.meta.env.VITE_API_URL;
 
-
   useEffect(() => {
     getAllPlaces();
   }, []);
 
   function getAllPlaces() {
-
     axios
       .get(`${API_URL}/api/places`)
       .then((response) => {
-        const placesFromApi = response.data.reverse(); //revert the order of the albums once a new one created appears as the first.
+        const placesFromApi = response.data; //revert the order of the albums once a new one created appears as the first.
         setPlaces(placesFromApi);
         //setFullAlbumsList(albumsFromApi); // Update full album list
       })
@@ -49,7 +47,6 @@ function PlacesList() {
   //     setAlbums(searchResult);
   //   }
 
-
   return (
     <>
       {/* <Search
@@ -58,24 +55,26 @@ function PlacesList() {
         getGenre={getGenre}
       /> */}
 
-<section className="grid gap-5 md:grid-cols-3 ml-10 mr-10 mb-10">
-  {places?.map((place) => (
-    <div key={place._id} className="w-full md:w-auto max-h-[400px]">
-      <Link to={`/places/${place._id}`}>
-        <div className="hover:bg-white p-4 rounded-3xl shadow-lg bg-slate-100 drop-shadow-xl h-full">
-          <div className="relative h-[200px] md:h-[300px] max-h-[400px] overflow-hidden rounded-3xl">
-            <img src={place.image} alt={place.name} className="w-full h-full object-cover" />
+      <section className="grid gap-8 md:grid-cols-3 ml-10 mr-10 mb-10">
+        {places?.map((place) => (
+          <div key={place._id} className="w-full md:w-auto max-h-[400px]">
+            <Link to={`/places/${place._id}`}>
+              <div className="hover:bg-white p-4 rounded-3xl shadow-lg bg-slate-100 drop-shadow-xl h-full">
+                <div className="relative h-[200px] md:h-[300px] max-h-[400px] overflow-hidden rounded-3xl">
+                  <img
+                    src={place.image[0]}
+                    alt={place.name}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="text-gray-950 text-center mt-2">
+                  {<div>{place.name}</div>}
+                </div>
+              </div>
+            </Link>
           </div>
-          <div className="text-center mt-2">
-{/*             <h1>{place.name}</h1>
-            <hr />
-            {place.description} */}
-          </div>
-        </div>
-      </Link>
-    </div>
-  ))}
-</section>
+        ))}
+      </section>
     </>
   );
 }
--- src/pages/PlaceDetailsPage.jsx
+++ src/pages/PlaceDetailsPage.jsx
@@ -3,59 +3,82 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 function PlaceDetailsPage() {
-    const [place, setPlace] = useState(null);
-    const { placeId } = useParams();
+  const [place, setPlace] = useState(null);
+  const { placeId } = useParams();
 
-    const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
 
+  useEffect(() => {
+    getPlaceDetails();
+  }, [placeId]);
 
-    useEffect(() => {
-        getPlaceDetails();
-    }, [placeId]);
+  const getPlaceDetails = () => {
+    axios
+      .get(`${API_URL}/api/places/${placeId}`)
+      .then((response) => {
+        const placeDetailsFromApi = response.data;
+        console.log(response.data);
+        setPlace(placeDetailsFromApi);
+      })
+      .catch((e) => console.log("error" + e));
+  };
+  return (
+    <>
+      {place && (
+        <div className="mb-10">
+          <section className="text-zinc-800 mx-auto mb-4 max-w-screen-md">
+            <div className="w-full md:w-auto">
+              <div className="hover:bg-white p-4 rounded-lg shadow-lg bg-slate-100 drop-shadow-xl">
+                <div className="text-center mt-2">
+                  <h1>{place.name}</h1>
+                  <hr />
+                  {place.description}
+                  {place.petsAllowed ? (
+                    <div>DOGS ALLOWED</div>
+                  ) : (
+                    <div>NO DOGS ALLOWED</div>
+                  )}
+                  {place.handicapAccessible ? (
+                    <div>ACCESSIBLE</div>
+                  ) : (
+                    <div>NOT ACCESSIBLE</div>
+                  )}
+                  {place.kitchenAvailable ? (
+                    <div>KITCHEN</div>
+                  ) : (
+                    <div>NO KITCHEN</div>
+                  )}
+                  {place.elevatorAvailable ? (
+                    <div>ELEVATOR</div>
+                  ) : (
+                    <div>NO ELEVATOR</div>
+                  )}
+                  {place.poolAvailable ? <div>POOL</div> : <div>NO POOL</div>}
+                  {place.isLuxurious ? (
+                    <div>LUXURIOUS</div>
+                  ) : (
+                    <div>TOTAL CRAP</div>
+                  )}
+                  {place.address}
+                  {place.location.coordinates}
+                </div>
+              </div>
 
-    const getPlaceDetails = () => {
-
-        axios
-            .get(`${API_URL}/api/places/${placeId}`)
-            .then((response) => {
-                const placeDetailsFromApi = response.data;
-                console.log(response.data)
-                setPlace(placeDetailsFromApi);
-            })
-            .catch((e) => console.log("error" + e));
-
-    };
-    return (
-        <>
-            {place && (
-                <section className="text-zinc-800 grid gap-5 md:grid-cols-2 ml-50 mr-50 mb-10">
-                    <div className="w-full md:w-auto">
-
-                        <div className="hover:bg-white p-4 rounded-lg shadow-lg bg-slate-100 drop-shadow-xl">
-                            <img src={place.image} alt={place.name} />
-                            <div className="text-center mt-2">
-                                <h1>{place.name}</h1>
-                                <hr />
-                                {place.description}
-                                {place.petsAllowed ? <div>DOGS ALLOWED</div> : <div>NO DOGS ALLOWED</div>}
-                                {place.handicapAccessible ? <div>ACCESSIBLE</div> : <div>NOT ACCESSIBLE</div>}
-                                {place.kitchenAvailable ? <div>KITCHEN</div> : <div>NO KITCHEN</div>}
-                                {place.elevatorAvailable ? <div>ELEVATOR</div> : <div>NO ELEVATOR</div>}
-                                {place.poolAvailable ? <div>POOL</div> : <div>NO POOL</div>}
-                                {place.isLuxurious ? <div>LUXURIOUS</div> : <div>TOTAL CRAP</div>}
-                                {place.address}
-                                {place.location.coordinates}
-                            </div>
-                        </div>
-                        <Link to={`/`}>
-                            <button>Homepage</button>
-                        </Link>
-                    </div>
-                </section>
-            )}
-        </>
-    )
-
-};
+              <div>
+                <img src={place.image[0]} alt={place.name} />
+                <img src={place.image[1]} alt={place.name} />
+                <img src={place.image[2]} alt={place.name} />
+                <img src={place.image[3]} alt={place.name} />
+              </div>
+            </div>
+          </section>
+          <Link to={`/`}>
+            <button>Back</button>
+          </Link>
+        </div>
+      )}
+    </>
+  );
+}
 
 export default PlaceDetailsPage;
