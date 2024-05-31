22ef426178145f58593f4255d52de535100eb731
Michael Giannini <giannini.michaelg@gmail.com>
2024-05-31 19:19:27 +0200

    update .env

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,10 +1,14 @@
 const GoogleMap = ({address}) => {
 
+  
+
+  const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+
     return (
       <div>
         <iframe
           title="google map"
-          src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBapC5bIrPygdzF8X4-pIbrSgfmpFSa3B8&q=${encodeURIComponent(address)}`}
+          src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${encodeURIComponent(address)}`}
           width="100%"
           height="500"
           style={{ border: 0 }}
