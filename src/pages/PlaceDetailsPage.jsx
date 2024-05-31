4cd812833e371832e55d613b6df5c6d675e0ef83
Christian Kuechler <ckuechler@gmail.com>
2024-05-31 12:47:10 +0200

    Add calendar. Create reservations for static user

--- src/pages/PlaceDetailsPage.jsx
+++ src/pages/PlaceDetailsPage.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function PlaceDetailsPage() {
   const [place, setPlace] = useState(null);
   const { placeId } = useParams();
-
+  const [date, setDate] = useState(new Date());
   const API_URL = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
@@ -22,6 +24,29 @@ function PlaceDetailsPage() {
       })
       .catch((e) => console.log("error" + e));
   };
+
+  const submitReservation = (date) => {
+    console.log("start " + date[0].toISOString());
+    console.log("end " + date[1].toISOString());
+
+    const dates = [date[0], date[1]];
+    const requestBody = {
+      name: place.name,
+      date: dates,
+      description: "can be removed?",
+      place: placeId,
+      user: "6657293dbbbb6f7ef765b721" // TODO: make dynamic
+    }
+    console.log("New reservation " + requestBody);
+
+    axios
+      .post(`${API_URL}/api/reservations`, requestBody)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((e) => console.log("error" + e));
+  };
+
   return (
     <>
       {place && (
@@ -57,12 +82,37 @@ function PlaceDetailsPage() {
                   {place.isLuxurious ? (
                     <div>LUXURIOUS</div>
                   ) : (
-                    <div>TOTAL CRAP</div>
+                    <div>STANDARD</div>
                   )}
                   {place.address}
                   {place.location.coordinates}
                 </div>
               </div>
+              <h1 className="text-gray-50">Reservation</h1>
+              <div className="flex items-center justify-center m-5">
+                <Calendar onChange={setDate} value={date} selectRange={true} />
+              </div>
+              {date.length > 0 ? (
+                <div>
+                  <p className="text-slate-50">
+                    <span className="bold">Start:</span>{" "}
+                    {date[0].toDateString()}
+                    &nbsp;|&nbsp;
+                    <span className="bold">End:</span> {date[1].toDateString()}
+                  </p>
+                  <button
+                    type="button"
+                    onClick={() => submitReservation(date)}
+                    className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                  >
+                    Book reservation
+                  </button>
+                </div>
+              ) : (
+                <p className="text-slate-50">
+                  <span className="bold">Please select a date range</span>
+                </p>
+              )}
 
               <div>
                 <img src={place.image[0]} alt={place.name} />
