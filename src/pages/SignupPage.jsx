f6369a20c814af88fbb9a8317c47ae5a37586b46
Christian Kuechler <ckuechler@gmail.com>
2024-06-04 12:22:04 +0200

    Import .env url

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-const API_URL = "http://localhost:5005";
+const API_URL = import.meta.env.VITE_API_URL;
 function SignupPage(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
