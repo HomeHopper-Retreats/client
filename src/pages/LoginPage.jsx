d67f79043bc1575460c09e03e878de7b3e59e30d
Christian Kuechler <ckuechler@gmail.com>
2024-06-05 17:46:27 +0200

    Partially styled login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,6 +10,7 @@ function LoginPage(props) {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
   const navigate = useNavigate();
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
   const { storeToken, authenticateUser } = useContext(AuthContext);
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
@@ -40,8 +41,7 @@ function LoginPage(props) {
 
   return (
     <div className="LoginPage">
-      <h1>Login</h1>
-      <form onSubmit={handleLoginSubmit}>
+      {/*       <form onSubmit={handleLoginSubmit}>
         <label>Email:</label>
         <input type="email" name="email" value={email} onChange={handleEmail} />
         <label>Password:</label>
@@ -52,11 +52,72 @@ function LoginPage(props) {
           onChange={handlePassword}
         />
         <button type="submit">Login</button>
-      </form>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
-      <button onClick={() => logout()}>Logout</button>
+      </form> */}
+
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      <div className="flex justify-center mt-10">
+        <div className="w-full max-w-xs">
+          <form
+            onSubmit={handleLoginSubmit}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={handleEmail}
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePassword}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              {!isLoggedIn && (
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Login
+                </button>
+              )}
+            </div>
+          </form>
+          {isLoggedIn && (
+            <button
+              className="h-10 px-5 m-2 text-red-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800"
+              onClick={() => logout()}
+            >
+              Logout
+            </button>
+          )}
+
+          <p>Don't have an account yet?</p>
+          <Link to={"/signup"}>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
